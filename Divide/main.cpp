8678dc07fe203618184b6fb69e5f5817d95541de
王可欣 <wangkexin@wangkexins-macbook-pro.local>
2019-02-28 15:58:04 +0800

    Initial Commit

---
+++ Divide/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Divide
+//
+//  Created by 王可欣 on 2019/2/28.
+//  Copyright © 2019年 王可欣. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
